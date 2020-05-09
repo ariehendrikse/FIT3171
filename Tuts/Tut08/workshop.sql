e8fac3292df90f97f7fa0a3410791d0fef2b9c1f
ariehendrikse <ahen0012@student.monash.edu>
2020-05-09 19:43:05 +1000

    Q2

--- Tuts/Tut08/workshop.sql
+++ Tuts/Tut08/workshop.sql
@@ -1,4 +1,11 @@
 -- Arie Hendrikse
 -- 9 May 2020
 --Q1
-select studid,studfname,studlname,studdob from uni.student order by studdob
+select studid,(studfname || ' ' || studlname) as NAME,to_char(studdob,'DD-MM-YYYY') AS dob from uni.student order by studdob;
+
+--Q2
+SELECT studid,(studfname || ' ' || studlname) as NAME,to_char(ofyear,'YYYY') AS year, semester
+FROM 
+   uni.student NATURAL JOIN uni.enrolment 
+WHERE mark IS NULL
+order by unitcode,studid;
