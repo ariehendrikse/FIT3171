004cd5e85ebe9b9d29bf6a826288d3e1f13c9cdc
ariehendrikse <ahen0012@student.monash.edu>
2020-05-10 17:10:12 +1000

    just in case i forgot to submit

--- Tuts/Tut07/tutorial.sql
+++ Tuts/Tut07/tutorial.sql
@@ -1,8 +1,10 @@
-drop table student purge;
-drop table unit purge;
-drop table enrolment purge;
+
 set echo on
 spool week7_schema_output.txt
+drop table enrolment purge;
+drop table student purge;
+drop table unit purge;
+
 
 create table student
     (
@@ -35,7 +37,37 @@ create table enrolment
         constraint stu_fk foreign key ( stu_nbr )
             references student ( stu_nbr ) on delete set null
     )   ;
+INSERT INTO student 
+    (
+        stu_nbr, stu_lname, stu_fname, stu_dob
+    )
+    VALUES
+        (11111111, 'Bloggs', 'Fred', '1-Jan-90'),
+        (11111112, 'Nice', 'Nick', '10-Oct-94'),
+        (11111113, 'Wheat', 'Wendy', '5-May-90'),
+        (11111114, 'Sheen', 'Cindy', '25-Dec-96')
+    ;
+    
+INSERT INTO unit 
+    (
+        stu_nbr, stu_lname, stu_fname, stu_dob
+    )
+    VALUES
+        ('FIT9999',     'FIT Last Unit'),
+        ('FIT5132',     'Introduction to Databases'),
+        ('FIT5016',     'Project'),
+        ('FIT5111',     'Students Life')
+    ;
+    
+INSERT INTO enrolment
+    (
+        (unit_code,unit_name)
+    )
+    VALUES
+        
 
+
+SELECT * FROM student
 commit;
 spool off
 set echo off
